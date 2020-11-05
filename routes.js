423b0ba6d1305cb6a8b657b83d96d6e4c53311c2
laurentsem <laurent.sem@edu.itescia.Fr>
2020-11-05 14:05:25 +0100

    Correction

--- routes.js
+++ routes.js
@@ -3,56 +3,66 @@ const router = Router()
 
 const store = {
     resources: {
-        res_id: {
+        'id_1': {
             name: 'xx'
         },
+        'id_2': {
+            name: 'yy'
+        }
     }
 }
 
-/*
-get /api/resources/res_id -> renvoi la resource store.resources['res_id']
-*/
-/*
-Exercice:
 
-4 Routes
-- 1) recuperer un objet avec son ID
-- 2) creer un objet 
-- 3) replace une resource avec son ID
-- 4) patch une resource avec son ID
-- 5) delete une resource son ID
-----------------------------------------
-
-Contraintes:
-- Le plus RESTful possible (methodes, json)
-- Un fichier de test:
-    _> ajoute, get, modifie, get, et supprime la ressource.
-*/
+// Get all resources
+router.get('/resources/', (req, res) => {
+    res.json(store.resources)
+})
 
 // Get resources by res_id
 router.get('/resources/:id', (req, res) => {
     res.json(store.resources[req.params.id])
 })
 
-// Delete resources by res_id
-router.delete('/resources/:id/', ((req, res) => {
-    if(!store.resources[req.params.id]) {
-        res.send('Resources not found ')
-    } else {
-        delete store.resources[req.params.id]
-        res.send('Resources delete')
-    }
-
-}))
-
+// Add new resource
 router.post('/resources/', (req, res) => {
-    console.log(req.body)
+    const resource = req.body
+    // Génère un id
+    resource.id = Object.keys(store.resources).length + 1
+    store.resources[resource.id] = resource
     res.send('Resources added !')
 })
 
-router.put('/resources/:id', ((req, res) => {
+// Modify one resource
+router.put('/resources/:id', (req, res) => {
+    if (req.params.id === req.body.id) {
+        store.resources[req.params.id] = req.body
+        res.json(req.body);
+    } else {
+        res.status(400).end();
+    }
+
+})
+
+// Patch one resource
+router.patch('/resources/:id', (req, res) => {
+    const id = req.params.id
+    const resource = { ...store.resources[id], ...req.body }
+    store.resources[id] = resource
+    res.json(resource)
+})
+
+// Delete resources by res_id
+router.delete('/resources/:id/', ((req, res) => {
+    // Par destructuration
+    const { id } = req.params
+    if(store.resources[id]) {
+        delete store.resources[id]
+        res.json({ success: true })
+    }
+    else {
+        res.status(404).end();
+    }
 
 }))
-
 
 module.exports = router
--- test.js
+++ test.js
@@ -1,5 +1,32 @@
 const axios = require('axios')
+const BASE_URL = 'http://localhost:4021'
 
 async function test_create(resource) {
-    const {data} = axios.post('http://')
+    const result = await axios.post(`${BASE_URL}/api/resources`, resource)
+    return result.data
 }
+
+async function test_get(id) {
+    const result = await axios.get(`${BASE_URL}/api/resources/${id}`)
+    return result.data
+}
+
+async function test_delete(id) {
+    const result = await axios.post(`${BASE_URL}/api/resources/${id}`)
+    return result.data
+}
+
+async function tests() {
+    const newResource = {
+        name: 'It is a real resource ?',
+        answer: 'maybe !'
+    }
+
+    const createResult = await test_create(newResource)
+    console.log({createResult})
+    const getResult1 = await test_get(createResult.id)
+    console.log({getResult1})
+
+}
+
+
